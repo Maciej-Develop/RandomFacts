c778d3d4f5f779c414130e341dfe4caa38d88886
Maciej Piechowski <58206@etu.he2b.be>
2025-03-25 16:55:33 +0100

    project created

---
+++ README.md
@@ -0,0 +1,7 @@
+# Random Facts (in developpement)
+
+## Technology
+
+    - Spring Boot v3.4.4
+    - Java v21
+    - Maven v3.9.9
---
+++ random-facts/src/main/java/maciej/develop/random_facts/RandomFactsApplication.java
@@ -0,0 +1,13 @@
+package maciej.develop.random_facts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomFactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomFactsApplication.class, args);
+	}
+
+}
