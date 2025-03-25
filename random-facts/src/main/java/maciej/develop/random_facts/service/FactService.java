cacb432777faf158f2ed70f3ffecf2492a6529a4
Maciej Piechowski <maciej.piechowski.dev@gmail.com>
2025-03-25 17:25:41 +0100

    updated properties + service

---
+++ random-facts/src/main/java/maciej/develop/random_facts/service/FactService.java
@@ -0,0 +1,26 @@
+package maciej.develop.random_facts.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import maciej.develop.random_facts.model.Fact;
+import maciej.develop.random_facts.repository.FactRepository;
+
+@Service
+public class FactService {
+
+    @Autowired
+    private FactRepository factRepository;
+
+    public void create(Fact fact) {
+        factRepository.save(fact);
+    }
+
+    public Iterable<Fact> getAll() {
+        return factRepository.findAll();
+    }
+
+    public Fact getById(Long id) {
+        return factRepository.findById(id).get();
+    }
+}
