22776c7f1d8d0b4b7005856de72ce215e63cbbbd
Maciej Piechowski <maciej.piechowski.dev@gmail.com>
2025-04-08 14:45:38 +0200

    display a error message

--- random-facts-client/src/app/home/home.component.ts
+++ random-facts-client/src/app/home/home.component.ts
@@ -4,22 +4,25 @@ import { catchError } from 'rxjs';
 import { FactComponent } from "../components/fact/fact.component";
 import { Fact } from '../model/fact.type';
 import { LoadingComponent } from '../components/loading/loading.component';
+import { ErrorComponent } from "../components/error/error.component";
 
 @Component({
   selector: 'app-home',
-  imports: [FactComponent, LoadingComponent],
+  imports: [FactComponent, LoadingComponent, ErrorComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit{
   service = inject(FactsService);
   ready = signal(false);
+  noError = signal(true);
   fact = signal<Fact>({id:0, description:""});
 
   ngOnInit(): void {
     this.service.getFactFromApi()
       .pipe(catchError((err) => {
         console.log(err);
+        this.noError.set(false);
         throw err;
       })).subscribe((fact) => {
         this.fact.set(fact);
